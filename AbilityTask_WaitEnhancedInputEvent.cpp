8bd09425a8a817aafb839fac0c3426c98ec2f227
m-ahmed-elbeskeri <131454742+m-ahmed-elbeskeri@users.noreply.github.com>
2025-03-24 18:08:16 +0000

    Create AbilityTask_WaitEnhancedInputEvent.cpp

---
+++ AbilityTask_WaitEnhancedInputEvent.cpp
@@ -0,0 +1,63 @@
+// Copyright 2021 Joseph "Narxim" Thigpen.
+
+
+#include "AbilityTask_WaitEnhancedInputEvent.h"
+
+
+UAbilityTask_WaitEnhancedInputEvent* UAbilityTask_WaitEnhancedInputEvent::WaitEnhancedInputEvent(UGameplayAbility* OwningAbility, const FName TaskInstanceName, UInputAction* InputAction, const ETriggerEvent TriggerEventType, const bool bShouldOnlyTriggerOnce)
+{
+	UAbilityTask_WaitEnhancedInputEvent* AbilityTask = NewAbilityTask<UAbilityTask_WaitEnhancedInputEvent>(OwningAbility, TaskInstanceName);
+	
+	AbilityTask->InputAction = InputAction;
+	AbilityTask->EventType = TriggerEventType;
+	AbilityTask->bTriggerOnce = bShouldOnlyTriggerOnce;
+	
+	return AbilityTask;
+}
+
+void UAbilityTask_WaitEnhancedInputEvent::Activate()
+{
+	Super::Activate();
+
+	if (!AbilitySystemComponent.Get() || !Ability || !InputAction.IsValid())
+	{
+		return;
+	}
+
+	const APawn* const AvatarPawn = Cast<APawn>(Ability->GetAvatarActorFromActorInfo());
+	const APlayerController* const PlayerController = AvatarPawn ? Cast<APlayerController>(AvatarPawn->GetController()) : nullptr;
+	
+	if (!PlayerController)
+	{
+		return;
+	}
+
+	EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerController->InputComponent);
+			
+	if (IsValid(EnhancedInputComponent.Get()))
+	{
+		EnhancedInputComponent->BindAction(InputAction.Get(), EventType, this, &UAbilityTask_WaitEnhancedInputEvent::EventReceived);
+	}
+}
+
+void UAbilityTask_WaitEnhancedInputEvent::EventReceived(const FInputActionValue& Value)
+{
+	if (bTriggerOnce && bHasBeenTriggered)
+	{
+		return;
+	}
+
+	bHasBeenTriggered = true;
+	
+	InputEventReceived.Broadcast(Value);
+}
+
+void UAbilityTask_WaitEnhancedInputEvent::OnDestroy(const bool bInOwnerFinished)
+{
+	if (IsValid(EnhancedInputComponent.Get()))
+	{
+		EnhancedInputComponent->ClearBindingsForObject(this);
+	}
+	
+	Super::OnDestroy(bInOwnerFinished);
+}
